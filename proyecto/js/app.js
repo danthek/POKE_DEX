1ff776c420230ae3382ccb2a18a84eed3d0ef00f
Delgado Orozco <jesus.b.delgado@accenture.com>
2023-10-17 09:51:43 -0600

    added neew styles

--- proyecto/js/app.js
+++ proyecto/js/app.js
@@ -2,8 +2,7 @@
 
 // con document.addEventListener() esperamos a que se cargue todo el html y luego el js
 document.addEventListener('DOMContentLoaded',()=>{
-  const pokeNum=randomPokemon()
-  fetchData(pokeNum)
+  mostrarCard()
 })
 
 function randomPokemon(){
@@ -11,24 +10,42 @@ function randomPokemon(){
 }
 
 // consumir la API de Pokemon
-const fetchData= async(pokeNum)=>{
+const fetchData= async()=>{
+  const pokeNum=randomPokemon()
   try {
     const res= await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeNum}`)
     // con axios no ocupamos transformar la api call a json, solo con fetch
     const data = await res.json()
-    mostrarCard(data)
+    console.log(data);
+    return data
   } catch (error) {
     console.log(error);
   }
 }
 
-const mostrarCard=(pokemon)=>{
+const mostrarCard=async()=>{
+const pokeData= await fetchData()
+const pokeValues={
+  image: pokeData.sprites.other.dream_world.front_default,
+  name: pokeData.species.name,
+  hp:pokeData.stats[0].base_stat,
+  exp:pokeData.base_experience,
+  abilitie:pokeData.abilities[0].ability.name,
+  types:pokeData.types[0].type.name,
+  version:pokeData.game_indices[0].version.name
+}
+console.log(pokeValues);
 const flex= document.querySelector('.flex') // donde va el template
 const template=document.getElementById('template-card').content // capturar template
 const cloneTemplate=template.cloneNode(true) // clon del template para no modificar el original
 const fragment = document.createDocumentFragment() // crea un  farmento en el DOM, en lugar de usar innerHtml, tambien sirve para loops
 //modificar la imagen del pokemon
-cloneTemplate.querySelector('.card-body-img').setAttribute('src',pokemon.sprites.other.dream_world.front_default) // usaria querySelectorAll si se repitiera la clase
+cloneTemplate.querySelector('.card-body-img').setAttribute('src',pokeValues.image) // usaria querySelectorAll si se repitiera la clase
+cloneTemplate.querySelector('.card-body-title').innerHTML=`${pokeValues.name} <span>${pokeValues.hp} hp</span>` 
+cloneTemplate.querySelector('.card-body-text').innerHTML=`${pokeValues.exp} Exp`
+cloneTemplate.querySelectorAll('.card-footer-social h3')[0].textContent=`${pokeValues.types}`
+cloneTemplate.querySelectorAll('.card-footer-social h3')[1].textContent=`${pokeValues.abilitie}`
+cloneTemplate.querySelectorAll('.card-footer-social h3')[2].textContent=`${pokeValues.version}`
 // pasar clon al fragment
 fragment.appendChild(cloneTemplate)
 // ahora nuestro framgment lo pasamos al flex
